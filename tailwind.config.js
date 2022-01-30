9f8fb57a1c5bb7e9a6a00c1b36571deb459f8090
edwintantawi <edwintantawi@gmail.com>
2022-01-30 11:19:32 +0700

    🔧 Enable JIT mode and adding some colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+  mode: 'jit',
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
@@ -9,6 +10,19 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#FFFFFF',
+      primary: '#4267C5',
+      secondary: '#F0F3FA',
+      black: '#1F1F1F',
+      gray: {
+        light: '#D9D9D9',
+        base: '#B4B4B4',
+        dark: '#626262',
       },
     },
   },
