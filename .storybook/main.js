de6d26b5b4f4c67d75cdba553ab9ca0ca343177b
edwintantawi <edwintantawi@gmail.com>
2022-02-03 00:25:55 +0700

    🔧 Change path aliases

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   stories: [
     '../components/**/*.stories.mdx',
@@ -6,4 +8,11 @@ module.exports = {
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: '@storybook/react',
   staticDirs: ['../public'],
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../'),
+    };
+    return config;
+  },
 };
