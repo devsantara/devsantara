61e1194d77ebe09d67ce6e1a48b85b840e37f31f
edwintantawi <edwintantawi@gmail.com>
2022-02-02 12:01:28 +0700

    💄 Use Navbar component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,13 @@
 import type { NextPage } from 'next';
-import Image from 'next/image';
 
 // components
-import { Title } from '@/components/Title';
-import { Paragraph } from '@/components/Paragraph';
+import { Navbar } from '@/components/Navbar/Navbar';
 
 const Home: NextPage = () => {
   return (
-    <div className="grid h-screen place-content-center text-center">
-      <figure className="relative mx-auto mb-2 h-10 w-10">
-        <Image
-          src="/brand/devsantara-logo.svg"
-          layout="fill"
-          alt="Devsantara"
-        />
-      </figure>
-      <Title>Devsantara</Title>
-      <Paragraph>Next JS, Typescript, TailwindCSS</Paragraph>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 };
 
