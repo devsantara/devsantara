a22b81f8250216f92b24393f6789c72b80418ee6
edwintantawi <edwintantawi@gmail.com>
2022-02-03 22:05:03 +0700

    💄 Fix spacing

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -14,7 +14,7 @@ const Header: FC<Props> = () => {
   return (
     <header>
       <Container>
-        <div className="flex flex-col items-center gap-y-2 py-16 text-left md:text-center lg:gap-y-5 xl:py-20 xl:text-center">
+        <div className="flex flex-col items-center gap-y-4 py-16 text-left md:text-center lg:gap-y-5 xl:pt-20 xl:pb-14 xl:text-center">
           <Title className="max-w-4xl">
             TEMPAT <Highlight>BELAJAR</Highlight> DAN{' '}
             <Highlight>BERKOMUNITAS</Highlight> BAGI{' '}
