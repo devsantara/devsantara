30e0dc632f199fe4d20269be01d217dbd68d3395
edwintantawi <edwintantawi@gmail.com>
2022-02-05 00:47:32 +0700

    📝 Add MatterMeta interface

--- types/index.ts
+++ types/index.ts
@@ -2,3 +2,16 @@ import { ReactChild } from 'react';
 
 export type Children = ReactChild | ReactChild[];
 export type LinkTarget = '_blank' | '_parent' | '_self' | '_top';
+export interface MatterMeta {
+  date: string;
+  description: string;
+  draft: true;
+  keywords: string[];
+  lastmod: string;
+  order: number;
+  preview: string;
+  slug: string;
+  theme: string;
+  title: string;
+  length?: number;
+}
