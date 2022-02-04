73043e31295c5cc60656d4dd483fcde7d7d58dc7
edwintantawi <edwintantawi@gmail.com>
2022-02-04 19:02:46 +0700

    ♻️ Use components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import clsx from 'clsx';
+import Link from 'next/link';
 
 // components
 import { Navbar } from '@/components/Navbar';
@@ -9,6 +10,10 @@ import { Container } from '@/components/Container';
 import { Img } from '@/components/Img';
 import { Paragraph } from '@/components/Paragraph';
 import { Footer } from '@/components/Footer';
+import { Screen } from '@/components/Screen';
+import { Title } from '@/components/Title';
+import { Highlight } from '@/components/Highlight';
+import { Main } from '@/components/Main';
 
 const features = [
   {
@@ -17,6 +22,7 @@ const features = [
     image: '/icons/feature-communities.svg',
     description:
       'Berjumpa dengan teman teman yang memiliki tujuan yang sama seperti kamu, bangun jaringan dengan mereka',
+    url: '/communities',
   },
   {
     id: 2,
@@ -24,6 +30,7 @@ const features = [
     image: '/icons/feature-academy.svg',
     description:
       'Pelajari skill teknologi kamu dengan media text yang akan mempercepat skill kamu di banding media video.',
+    url: '/academy',
   },
   {
     id: 3,
@@ -31,15 +38,28 @@ const features = [
     image: '/icons/feature-resourses.svg',
     description:
       'Temukan sumber daya pembelajaran kamu di sini yang membantu kamu berkembang lebih cepat',
+    url: '/resourses',
   },
 ];
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col">
+    <Screen>
       <Navbar />
-      <Header />
-      <main className="flex-1">
+      <Header>
+        <Title className="max-w-4xl">
+          TEMPAT <Highlight>BELAJAR</Highlight> DAN{' '}
+          <Highlight>BERKOMUNITAS</Highlight> BAGI{' '}
+          <Highlight>
+            DEVELOPER <u>INDONESIA</u>
+          </Highlight>
+        </Title>
+        <Paragraph className="max-w-lg lg:max-w-2xl">
+          Pelajari keterampilan yang sangat dibutuhkan, dan berjumpa dengan
+          teman hingga mentor yang berpengalaman di bidangnya
+        </Paragraph>
+      </Header>
+      <Main>
         <Container>
           <div className="relative grid grid-cols-1 gap-x-5 gap-y-3 xl:mt-16 xl:grid-cols-3">
             {features.map((feature, index) => (
@@ -48,7 +68,7 @@ const Home: NextPage = () => {
                 className={clsx(
                   'px-5 py-7 text-center sm:px-20 lg:px-60 xl:p-9',
                   {
-                    'relative xl:-top-16': index === 1,
+                    'relative -order-1 xl:-top-16 xl:order-none': index === 1,
                   }
                 )}
               >
@@ -57,17 +77,21 @@ const Home: NextPage = () => {
                   alt="communities"
                   className="mx-auto mb-2 h-8 w-8 lg:h-12 lg:w-12"
                 />
-                <h2 className="mb-2 text-lg font-medium text-primary lg:text-xl">
-                  {feature.title}
-                </h2>
+                <Link href={feature.url}>
+                  <a>
+                    <h2 className="mb-2 text-lg font-medium text-primary lg:text-xl">
+                      {feature.title}
+                    </h2>
+                  </a>
+                </Link>
                 <Paragraph>{feature.description}</Paragraph>
               </Card>
             ))}
           </div>
         </Container>
-      </main>
+      </Main>
       <Footer />
-    </div>
+    </Screen>
   );
 };
 
