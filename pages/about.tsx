44f91214402d4119cf114282343e40841f35cc90
edwintantawi <edwintantawi@gmail.com>
2022-02-04 19:02:27 +0700

    💄 Add pages

---
+++ pages/about.tsx
@@ -0,0 +1,27 @@
+import { NextPage } from 'next';
+
+// components
+import { Header } from '@/components/Header';
+import { Highlight } from '@/components/Highlight';
+import { Navbar } from '@/components/Navbar';
+import { Paragraph } from '@/components/Paragraph';
+import { Screen } from '@/components/Screen';
+import { Title } from '@/components/Title';
+
+const About: NextPage = () => {
+  return (
+    <Screen>
+      <Navbar />
+      <Header>
+        <Title className="max-w-4xl">
+          DEVSANTARA <Highlight>ABOUT US</Highlight>
+        </Title>
+        <Paragraph className="md:max-w-lg lg:max-w-2xl">
+          Tempat Belajar dan Berkomunitas bagi Developer Indonesia
+        </Paragraph>
+      </Header>
+    </Screen>
+  );
+};
+
+export default About;
