0c454d7e2106a230c801623e129c02b4a893807e
edwintantawi <edwintantawi@gmail.com>
2022-02-05 12:32:17 +0700

    ✨ Use markdown-it to render markdown

--- pages/academy/[academy]/[module].tsx
+++ pages/academy/[academy]/[module].tsx
@@ -1,14 +1,55 @@
-import matter from 'gray-matter';
 import { GetStaticPropsContext, NextPage } from 'next';
+import matter from 'gray-matter';
 import path from 'path';
+import MarkdownIt from 'markdown-it';
+import hljs from 'highlight.js';
 
-import { readDir, readFile } from '@/utils';
 import { MatterMeta } from '@/types';
+import { readDir, readFile } from '@/utils';
 
-interface Props extends MatterMeta {}
+// components
+import { Screen } from '@/components/Screen';
+import { Navbar } from '@/components/Navbar';
+import { Footer } from '@/components/Footer';
+import { Main } from '@/components/Main';
+import { Container } from '@/components/Container';
+import { SideBar } from '@/components/SideBar';
+import { Content } from '@/components/Content';
 
-const Module: NextPage<Props> = ({ title }) => {
-  return <div>{title}</div>;
+interface Props extends MatterMeta {
+  academyModules: MatterMeta[];
+}
+
+const Module: NextPage<Props> = ({
+  title,
+  theme,
+  preview,
+  academyModules,
+  content,
+  slug,
+}) => {
+  const academySlug = slug.split('/').slice(0, -1).join('/');
+
+  return (
+    <Screen>
+      <Navbar />
+      <Main>
+        <Container>
+          <div className="mt-16 grid grid-cols-1 gap-x-5 xl:grid-cols-[280px,1fr]">
+            <SideBar
+              theme={theme}
+              preview={preview}
+              academyModules={academyModules}
+              academySlug={academySlug}
+              className="sticky top-20 hidden h-fit xl:block"
+            />
+            <Content title={title} content={content} />
+          </div>
+        </Container>
+      </Main>
+      <Footer />
+    </Screen>
+  );
 };
 
 interface Context extends GetStaticPropsContext {
@@ -25,11 +66,39 @@ export const getStaticProps = async ({ params }: Context) => {
   const moduleFile = await readFile(
     path.join(basePath, academy, `${module}.md`)
   );
-  const matterMeta = matter(moduleFile).data;
+
+  const academyModulesFile = await readDir(path.join(basePath, academy), {
+    exclude: 'index.md',
+  });
+
+  const academyModulesPromises = academyModulesFile.map(async (module) => {
+    const file = await readFile(path.join(basePath, academy, module));
+    return matter(file).data;
+  });
+
+  const academyModules = await Promise.all(academyModulesPromises);
+
+  const matterResult = matter(moduleFile);
+  const matterMeta = matterResult.data;
+  const matterContent = matterResult.content;
+
+  const md = new MarkdownIt({
+    highlight: function (str, lang) {
+      if (lang && hljs.getLanguage(lang)) {
+        try {
+          return hljs.highlight(str, { language: lang }).value;
+        } catch (__) {}
+      }
+
+      return '';
+    },
+  });
 
   return {
     props: {
       ...matterMeta,
+      content: md.render(matterContent),
+      academyModules,
     },
   };
 };
