e0dc06da0efcf6e1b52a7d049879193d90c6559d
edwintantawi <edwintantawi@gmail.com>
2022-02-06 18:09:21 +0700

    🔍 Create AppHead component

---
+++ components/AppHead/AppHead.tsx
@@ -0,0 +1,47 @@
+import { FC } from 'react';
+import Head from 'next/head';
+
+import { Children } from '@/types';
+
+interface Props {
+  children?: Children;
+  title?: string;
+  keywords?: string[];
+  description?: string;
+  preview?: string;
+  theme?: string;
+}
+
+const defaultKeywords = [
+  'devsantara',
+  'developer nusantara',
+  'belajar programming',
+  'developer',
+];
+
+const AppHead: FC<Props> = ({
+  children,
+  title = 'Devsantara | Developer Nusantara',
+  keywords = defaultKeywords,
+  description = 'Pelajari keterampilan yang sangat dibutuhkan di masa depan dan berkomunitas bersama Devsantara.',
+  preview = '/images/brand.png',
+  theme = '#4267C5',
+}) => {
+  const keywordsString = [...defaultKeywords, ...keywords]?.join(', ');
+
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+      <meta name="keywords" content={keywordsString} />
+      <meta name="description" content={description} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={preview} />
+      <meta name="theme-color" content={theme} />
+      {children}
+    </Head>
+  );
+};
+
+export { AppHead };
