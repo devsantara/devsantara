9eaf80401448f9da33922a9f652fb721394c1884
edwintantawi <edwintantawi@gmail.com>
2022-02-06 18:11:00 +0700

    ♻️ Refactor code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -39,10 +39,9 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
             rel="stylesheet"
           />
+          {/* favicon */}
+          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         </Head>
-
-        {/* favicon */}
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <body>
           <Main />
           <NextScript />
