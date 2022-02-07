21d649b1d88b8fb4e7589e31ab6cfd1baccaf29e
edwintantawi <edwintantawi@gmail.com>
2022-02-07 11:00:59 +0700

    ✨ Implement gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,27 @@
+import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+
+import { gtagPageview } from '@/libs';
 
 // styles
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouterChange = (url: URL) => {
+      gtagPageview(url);
+    };
+
+    router.events.on('routeChangeComplete', handleRouterChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouterChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 };
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,8 @@ import Document, {
   NextScript,
 } from 'next/document';
 
+import { GA_MEASUREMENT_ID } from '@/libs';
+
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -41,6 +43,28 @@ class MyDocument extends Document {
           />
           {/* favicon */}
           <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+
+          {/* Global site tag (gtag.js) - Google Analytics */}
+          {process.env.NODE_ENV === 'production' && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${GA_MEASUREMENT_ID}', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+                }}
+              />
+            </>
+          )}
         </Head>
         <body>
           <Main />
